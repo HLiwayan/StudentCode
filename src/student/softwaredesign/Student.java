a218f29d89091e3802cbbd3554a35c99ee11bf43
HLiwayan <liwayanh@sheridancollege.ca>
2019-01-25 12:38:22 -0500

    Student Commit

---
+++ src/student/softwaredesign/Student.java
@@ -0,0 +1,34 @@
+package student.softwaredesign;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Harry_Liwayan
+ */
+public class Student 
+{
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public int getStuID() {
+        return stuID;
+    }
+
+    public void setStuID(int stuID) {
+        this.stuID = stuID;
+    }
+    private String stuName;
+    private int stuID;
+    
+}
